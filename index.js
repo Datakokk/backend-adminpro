7464c786b9cbc0aa69dc7a4e2f2fd0de55c0bf38
Fernando <fiodorelzarster@gmx.com>
2022-01-22 12:08:44 +0100

    the route file for hospital has been added in the index

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ dbConnection();
 // Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/hospitals', require('./routes/hospitals'));
+app.use('/api/doctors', require('./routes/doctors'));
 app.use('/api/login', require('./routes/auth'));
 
 
