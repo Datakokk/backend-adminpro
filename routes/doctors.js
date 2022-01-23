1451ab932a23cee42c780260a38ba600df6496be
Fernando <fiodorelzarster@gmx.com>
2022-01-23 19:17:57 +0100

    Router doctor and hospital have modified, middleware has been added

--- routes/doctors.js
+++ routes/doctors.js
@@ -7,13 +7,20 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { getDoctors, createDoctors, updateDoctors, deleteDoctor } = require('../controllers/doctor');
+const { fieldsValidation } = require('../middlewares/field-validation');
+const { validateJWT } = require('../middlewares/jwt-validation');
 
 const router = Router();
 
 router.get( '/', getDoctors);
 
 router.post( '/',
-    [],
+    [
+        validateJWT,
+        check('name', 'The Doctor name is mandatory').not().isEmpty(),
+        check('hospital', 'The hospital id must be valid').isMongoId(),
+        fieldsValidation
+    ],
     createDoctors
 );
 
--- routes/hospitals.js
+++ routes/hospitals.js
@@ -6,6 +6,8 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { getHospitals, createHospitals, updateHospitals, deleteHospital } = require('../controllers/hospital');
+const { fieldsValidation } = require('../middlewares/field-validation');
+const { validateJWT } = require('../middlewares/jwt-validation');
 
 
 
@@ -14,12 +16,18 @@ const router = Router();
 router.get( '/', getHospitals);
 
 router.post( '/', 
-    [],
+    [
+        validateJWT,
+        check('name', 'The hospital name is mandatory').not().isEmpty(), 
+        fieldsValidation
+    ],
     createHospitals
 );
 
 router.put( '/:id', 
-    [],
+    [
+        
+    ],
     updateHospitals
 );
 
