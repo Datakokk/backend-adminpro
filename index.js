382f47e952685f933c1c777e63208d490a184dee
Fernando <fiodorelzarster@gmx.com>
2022-01-23 19:20:20 +0100

    The routes created have been added to the index

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ dbConnection();
 app.use('/api/users', require('./routes/users'));
 app.use('/api/hospitals', require('./routes/hospitals'));
 app.use('/api/doctors', require('./routes/doctors'));
+app.use('/api/searchs', require('./routes/searchs'));
 app.use('/api/login', require('./routes/auth'));
 
 
