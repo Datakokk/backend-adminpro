3ea0e41cca8021791ddca7880fa601e583e13b9a
Fernando <fiodorelzarster@gmx.com>
2022-01-29 18:06:29 +0100

    helpers 'jwt' and 'update-image' creted

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -27,3 +27,8 @@ const generateJWT = ( uid ) => {
 module.exports = {
     generateJWT,
 }
+
+
+// var serveIndex = require('serve-index');
+// app.use(express.static(__dirname + '/'))
+// app.use('/uploads', serveIndex(__dirname + '/uploads'));
