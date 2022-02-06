c56d8b8683a2b436dbfe6c2b451b6ab24682878c
Fernando <fiodorelzarster@gmx.com>
2022-02-06 12:01:50 +0100

    authentication created using google

--- helpers/google-verify.js
+++ helpers/google-verify.js
@@ -11,9 +11,6 @@ const googleVerify = async( token ) => {
       //[process.env.GOOGLE_ID_1, process.env.GOOGLE_ID_2, process.env.GOOGLE_ID_3]
   });
   const payload = ticket.getPayload();
-  const userid = payload['sub'];
-
-  console.log(payload);
   const { name, email, picture } = payload;
 
   return { name, email, picture };
